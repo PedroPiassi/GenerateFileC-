40231b72c6844ed56afa8ef29f2e9cf81876c2cb
pedro-piassi <pedro.piassi@rehagro.edu.br>
2024-09-18 22:07:21 -0300

    first commit

---
+++ TesteArquivos/Program.cs
@@ -0,0 +1,29 @@
+﻿using TesteArquivos.Entidades;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        List<Pessoa> listaDeObjetos = new List<Pessoa>
+            {
+                new Pessoa { Nome = "João", Idade = 30, Profissao = "Engenheiro" },
+                new Pessoa { Nome = "Maria", Idade = 25, Profissao = "Professora" },
+                new Pessoa { Nome = "Pedro", Idade = 40, Profissao = "Arquiteto" }
+            };
+
+        string timestamp = DateTime.Now.ToString("yyyy_MM_dd_HH_mm");
+        string caminhoArquivo = @$"C:\programas\arquivosteste\objetos_{timestamp}.txt";
+
+        // Abre o arquivo com o nome dinâmico
+        using (StreamWriter writer = new StreamWriter(caminhoArquivo))
+        {
+            foreach (var objeto in listaDeObjetos)
+            {
+                // Para cada objeto, escreve suas propriedades no arquivo
+                writer.WriteLine($"Nome: {objeto.Nome}, Idade: {objeto.Idade}, Profissão: {objeto.Profissao}");
+            }
+        }
+
+        Console.WriteLine($"Objetos gravados no arquivo '{caminhoArquivo}' com sucesso!");
+    }
+}
